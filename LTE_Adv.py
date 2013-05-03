93fa3b200a2736d4a03648513b0a3a24f583b1d2
Aladdin Chang <sp0500@gmail.com>
2013-05-03 16:22:43 +0800

    Update LTE_Adv.py

--- LTE_Adv.py
+++ LTE_Adv.py
@@ -6,7 +6,11 @@ f = urllib.urlopen("http://ad.studioclassroom.com/Ad-RAd.php")
 # Read from the object, storing the page's contents in 's'.
 s = f.read()
 f.close()
-print s
+#print s
+
+today=os.popen('date +%Y%m%d').read()
+print today
+
 obj = re.findall(r'href[\s]+=[\s]+?\"(mms://203.69.69.81/.+?ada.+?\.wma)\">?', s)
 for i in range(len(obj)):
     obj[i] = obj[i].replace("mms","http")
