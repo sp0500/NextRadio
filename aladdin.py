63491f2d8d425cacb5b13d57dfd2d88b409398b5
Aladdin Chang <sp0500@gmail.com>
2013-05-06 09:31:46 +0800

    Add LTE, Studio Classromm, Advanced

--- aladdin.py
+++ aladdin.py
@@ -31,8 +31,6 @@ def Say_station(st):
     #os.system("mplayer -ao oss -quiet " + prompt + " < /dev/null  ")
     return prompt
 
-
-
 def PlayRadio(station):
     print station, stationlist[station]
     os.system("killall " + "mplayer");
@@ -41,10 +39,8 @@ def PlayRadio(station):
     time.sleep(1)
     if 'pls' in stationlist[station] or 'asx' in stationlist[station]:
         Play_station = "mplayer -ao oss -slave -quiet -cache 256 " + prompt + " -playlist " + stationlist[station].rstrip("\n") + " < /dev/null &"
-        #Play_station = "mplayer -ao oss -slave -quiet -cache 256 -playlist " + stationlist[station].rstrip("\n") + " < /dev/null &"
     else:
         Play_station = "mplayer -ao oss -slave -quiet -cache 256 " + prompt + " " + stationlist[station].rstrip("\n") + " < /dev/null &"
-        #Play_station = "mplayer -ao oss -slave -quiet -cache 256 " + stationlist[station].rstrip("\n") + " < /dev/null &"
     print Play_station
     player = subprocess.Popen(Play_station.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE)
     return player
@@ -84,6 +80,7 @@ def ReadKey():
             elif (cmd_list[2] == "home"): # play favor station
                 return 1007
 
+
 i = 0
 stationlist=[]
 funkey = 0
@@ -92,18 +89,45 @@ max_stations = 0
 current_station = 0
 sockfile = '/dev/lircd'
 
-# Get a file-like object for the Python Web site's home page.
-f = urllib.urlopen("http://www.studioclassroom.com/default.php")
-# Read from the object, storing the page's contents in 's'.
+# Get LTE
+f = urllib.urlopen("http://lt.studioclassroom.com/LT-RaDio.php")
 s = f.read()
 f.close()
+obj_lta = re.findall(r'href[\s]+=[\s]+?\"(mms://203.69.69.81/.+?lta.+?\.wma)\">?', s)
+for i in range(len(obj_lta)):
+    obj_lta[i] = obj_lta[i].replace("mms","http")
 
-obj = re.findall(r'href=\"(http://203.69.69.81/[\s\S]+?\.wma)', s)
-for j in range(len(obj)):
-    stationlist.append(obj[j])
-    print obj[j]
-    i = i+1
+# Get Studio Classroom
+f = urllib.urlopen("http://sc.studioclassroom.com/Sc-rD.php")
+s = f.read()
+f.close()
+obj_baa = re.findall(r'href[\s]+=[\s]+?\"(mms://203.69.69.81/.+?baa.+?\.wma)\">?', s)
+for i in range(len(obj_baa)):
+    obj_baa[i] = obj_baa[i].replace("mms","http")
 
+# Get Advenced
+f = urllib.urlopen("http://ad.studioclassroom.com/Ad-RAd.php")
+s = f.read()
+f.close()
+obj_ada = re.findall(r'href[\s]+=[\s]+?\"(mms://203.69.69.81/.+?ada.+?\.wma)\">?', s)
+for i in range(len(obj_ada)):
+    obj_ada[i] = obj_ada[i].replace("mms","http")
+
+stationlist.append(obj_lta[3])
+stationlist.append(obj_lta[2])
+stationlist.append(obj_lta[1])
+stationlist.append(obj_baa[3])
+stationlist.append(obj_baa[2])
+stationlist.append(obj_baa[1])
+stationlist.append(obj_ada[3])
+stationlist.append(obj_ada[2])
+stationlist.append(obj_ada[1])
+
+i = 9;
+print stationlist
+
+today = os.popen('date +%Y%m%d').read()
+print today
 
 fd = open ("/root/NextRadio/all.pls","r")
 for line in fd:
