a464c640102d3edb628ca6c28195ecc1937993ac
Aladdin Chang <sp0500@gmail.com>
2013-05-17 14:11:14 +0800

    Add SC_rec.py

---
+++ SC_rec.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os, sys, re, urllib
+
+today = os.popen('date +%Y%m%d').read()
+
+f = urllib.urlopen("http://sc.studioclassroom.com/Sc-rD.php")
+s = f.read()
+f.close()
+obj = re.findall(r'href[\s]+=[\s]+?\"(mms://203.69.69.81/.+?baa.+?\.wma)\">?', s)
+if obj:
+    os.system("mimms -q %s %s_baa.wma"%(obj[0],today.rstrip("\n")))
+
+f = urllib.urlopen("http://lt.studioclassroom.com/LT-RaDio.php")
+s = f.read()
+f.close()
+obj = re.findall(r'href[\s]+=[\s]+\"(mms://203.69.69.81/[\s\S]+?lta[\s\S]+?\.wma)', s)
+if obj:
+    os.system("mimms -q %s %s_lta.wma"%(obj[0],today.rstrip("\n")))
+
+f = urllib.urlopen("http://ad.studioclassroom.com/Ad-RAd.php")
+s = f.read()
+f.close()
+obj = re.findall(r'href[\s]+=[\s]+?\"(mms://203.69.69.81/.+?ada.+?\.wma)\">?', s)
+if obj:
+    os.system("mimms -q %s %s_ada.wma"%(obj[0],today.rstrip("\n")))
+
